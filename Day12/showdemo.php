b834392156e664a6c6b9afcc9345eca98219ebba
64309010019 <64309010019@chontech.com>
2021-12-02 16:33:05 +0700

    day5

--- Day12/showdemo.php
+++ Day12/showdemo.php
@@ -18,7 +18,7 @@ $result = mysqli_query($conn, $sql);
 if (mysqli_num_rows($result) > 0) {
   // output data of each row
   while($row = mysqli_fetch_assoc($result)) {
-    echo "id: " . $row["id"]. " - Name: " . $row["name"]. " " . $row["address"]. "<br>";
+    echo "Id: " . $row["id"]. " - Name: " . $row["name"]. "- Address " . $row["address"]. "<br>";
   }
 } else {
   echo "0 results";
